cb027f49f68c795bb554a5c11e1e9cd7caa8dca6
Anwar, Malik Aqeel <manwar8@gatech.edu>
2020-06-28 18:18:17 -0400

    Update analysis_functions.py

--- analysis_functions.py
+++ analysis_functions.py
@@ -16,15 +16,22 @@ def crawl_the_chat(chat):
     # Each message is stored as a row in the dataframe
     # To identify messages, regular expressions are used to identify dates format
     # Depending on the user's mobile clock settings, there existing two clock patterns
+
     print("Crawling the chat")
     # pattern_time_24hr = ", (0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9])"
     # pattern_time_12hr = ", (0?[0-9]|1[0-2]):([0-9]|[0-5][0-9]) [AP]M"
 
-    pattern_time_24hr = ", (0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?"
-    pattern_time_12hr = ", (0?[0-9]|1[0-2]):([0-9]|[0-5][0-9])(:[0-5][0-9])? [APap][Mm]"
+    pattern_time_24hr = ",? (0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?"
+    pattern_time_12hr = (
+        ",? (0?[0-9]|1[0-2]):([0-9]|[0-5][0-9])(:[0-5][0-9])? [APap][Mm]"
+    )
 
-    pattern_date_US = "(0?[1-9]|1[0-2])[/.](0?[1-9]|[12][0-9]|3[01])[/.](\d{2}|\d{4}), "
-    pattern_date_UK = "([12][0-9]|3[01]|0?[1-9])/(0?[1-9]|1[0-2])/(\d{2}|\d{4}), "
+    pattern_date_US = (
+        "(0?[1-9]|1[0-2])[/.-](0?[1-9]|[12][0-9]|3[01])[/.-](\d{2}|\d{4}),? "
+    )
+    pattern_date_UK = (
+        "([12][0-9]|3[01]|0?[1-9])[/.-](0?[1-9]|1[0-2])[/.-](\d{2}|\d{4}),? "
+    )
 
     # pattern_date_US = "(0?[1-9]|1[0-2])/(0?[1-9]|[12][0-9]|3[01])/\d\d"
     # pattern_date_UK = "([12][0-9]|3[01]|0?[1-9])/(0?[1-9]|1[0-2])/\d\d"
@@ -37,6 +44,8 @@ def crawl_the_chat(chat):
     dp = re.compile(pattern_date_UK)
     for d in dp.finditer(chat):
         date = d.group()
+        date = date.replace("-", "/")
+        date = date.replace(".", "/")
         if int(date.split("/")[0]) > 12:
             pattern_date = pattern_date_UK
             is_UK = True
@@ -44,30 +53,43 @@ def crawl_the_chat(chat):
     if not is_UK:
         pattern_date = pattern_date_US
 
-    if re.search(pattern_time_12hr, chat):
+    is_12hr = False
+    # if re.search(pattern_time_12hr, chat):
+    if len(re.findall(pattern_time_12hr, chat)) > 50:
+        is_12hr = True
         pattern_time = pattern_time_12hr
     else:
         pattern_time = pattern_time_24hr
 
-    pattern = pattern_date + pattern_time[2:]
-    pattern_time = pattern_time[2:]
+    pattern = pattern_date + pattern_time[3:]
+    pattern_time = pattern_time[3:]
     p = re.compile(pattern)
+    cc = re.findall(pattern, chat)
     for m in p.finditer(chat):
         DateTime = m.group()
         # Split dateTime
         time = re.search(pattern_time, DateTime).group()
         date = re.search(pattern_date, DateTime).group()
-        date = date[:-2]
+        if "," in date:
+            date = date[:-2]
+        date = date.replace("-", "/")
+        date = date.replace(".", "/")
         if is_UK:
             spp = date.split("/")
             date = spp[1] + "/" + spp[0] + "/" + spp[2]
-
-        if len(date.split("/")[-1]) > 2:
+        # date = date.replace(' ', '')
+        print(date)
+        date = date.replace(" ", "")
+        if len(date.split("/")[-1]) == 4:
             date_split = date.split("/")
             if len(date_split) < 3:
                 date_split = date.split(".")
+            print(date)
             date = date_split[0] + "/" + date_split[1] + "/" + date_split[2][2:4]
 
+
+        print(date)
+        print('----------------')
         day_of_week = day_of_week_labels[T.strptime(date, "%m/%d/%y").tm_wday]
 
         if last_message_end > 0:
